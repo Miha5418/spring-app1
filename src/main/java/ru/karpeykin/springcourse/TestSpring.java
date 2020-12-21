0f1fc64c99cdfc130aa39478cf9134a0851a463d
karpeykin <karpejkin2016@gmail.com>
2020-12-21 15:48:20 +0700

    Lesson 7 added property "prototype"

--- src/main/java/ru/karpeykin/springcourse/TestSpring.java
+++ src/main/java/ru/karpeykin/springcourse/TestSpring.java
@@ -1,22 +1,41 @@
 package ru.karpeykin.springcourse;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
-    * @author Mihail
-*/
+ * @author Mihail
+ */
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        //Music music = context.getBean("musicBean", Music.class);
-        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayerFirst = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayerSecond = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = musicPlayerFirst == musicPlayerSecond;
+
+        System.out.println(comparison);
+
+        System.out.println(musicPlayerFirst);
+        System.out.println(musicPlayerSecond);
+
+        musicPlayerFirst.setVolume(10);
+
+        System.out.println(musicPlayerFirst.getVolume());
+        System.out.println(musicPlayerSecond.getVolume());
+        /*
+        musicPlayerFirst.playMusic();
+        System.out.println("1 name: " + musicPlayerFirst.getName()+ " volume: " + musicPlayerFirst.getVolume());
+
+        musicPlayerSecond.setVolume(90);
+        musicPlayerFirst.playMusic();
+        System.out.println("2 name: " + musicPlayerSecond.getName()+ " volume: " + musicPlayerSecond.getVolume());
+*/
         context.close();
     }
 }
