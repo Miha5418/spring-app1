c3343139afe505d5ed727b78349c145240a3f606
Karpeykin Mihail <karpejkin2016@yandex.ru>
2020-12-23 21:48:33 +0700

    lesson 11

--- src/main/java/ru/karpeykin/springcourse/MusicPlayer.java
+++ src/main/java/ru/karpeykin/springcourse/MusicPlayer.java
@@ -1,6 +1,7 @@
 package ru.karpeykin.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,19 +9,17 @@ import org.springframework.stereotype.Component;
  */
 @Component("musicPlayer")
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
-
-    // IoC
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    private MusicPlayer(@Qualifier("classicalMusicBean") Music music1,
+                        @Qualifier("rockMusicBean") Music music2){
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong() + " and " + rockMusic.getSong();
+        return "Playing: " + music1.getSong() + music2.getSong();
     }
 }
